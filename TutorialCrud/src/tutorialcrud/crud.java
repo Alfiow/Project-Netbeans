43b7bf2ca8e8e1a1f719533037d0fa9328a27084
Alfioww <alfianan0412@bsi.ac.id>
2017-05-05 14:56:04 +0700

    Program CRUD dasar java netbeans

---
+++ TutorialCrud/src/tutorialcrud/crud.java
@@ -0,0 +1,11 @@
+package tutorialcrud;
+
+public class crud {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmUtama as = new FrmUtama();
+        as.setVisible(true);
+    }
+    
+}
